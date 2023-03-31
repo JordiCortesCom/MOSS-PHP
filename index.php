8d6b1f081cc245aa0fbfdf9607e63057f96ae3f4
jordi cortes comellas <jordic@MacBook-MacBook-Pro-de-jordi.local>
2023-03-31 15:46:33 +0200

    ruta arreglada

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 include("moss.php");
-$userid = ""; // Enter your MOSS userid
+$userid = "407756086"; // Enter your MOSS userid
+
 $moss = new MOSS($userid);
-$moss->setLanguage('java');
-$moss->addByWildcard('test/*');
-$moss->addBaseFile('Example.java');
+
+# Elegir llenguatge entre: C, C++, Java, C#, Python, Visual Basic, Javascript, 
+# FORTRAN, ML, Haskell, Lisp, Scheme, Pascal, Modula2, Ada, Perl, TCL, Matlab, VHDL, 
+# Verilog, Spice, MIPS assembly, a8086 assembly, a8086 assembly, MIPS assembly, HCL2. 
+$moss->setLanguage('javascript'); 
+
+# Especificar ruta
+$moss->addByWildcard('./codiDeProva/*.php');
+
+# Conté directoris per alumnes?
+$moss->setDirectoryMode(false);
+
+# Seleccionar projecte base
+# $moss->addBaseFile('Example.java');
+
+# Incloure comentaris a la prova
 $moss->setCommentString("This is a test");
-print_r($moss->send());
+
+
+#print_r($moss->send());
+header("Location: " . $moss->send());
 ?>
